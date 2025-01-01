c06ca35c49fffd83c9df79da37a1ece00d6826a3
kuangyuwu <kuangyuwu95@gmail.com>
2025-01-01 15:19:53 +0800

    Ch3 L1 Exchanges and Queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,6 +21,23 @@ func main() {
 	}
 	defer conn.Close()
 	log.Println("connected to RabbitMQ server")
+
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("error creating channel: %s", err)
+	}
+
+	err = pubsub.PublishJSON(
+		publishCh,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Printf("could not publish json: %s", err)
+	}
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
